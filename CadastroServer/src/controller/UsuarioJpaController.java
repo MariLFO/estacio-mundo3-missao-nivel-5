f66aba3288afcaa3f8001654c10557e101c4120b
MariLFO <mari.fer.onorio@gmail.com>
2023-09-02 21:07:03 -0700

    Adicionado metodo findUsuario com login e senha

--- CadastroServer/src/controller/UsuarioJpaController.java
+++ CadastroServer/src/controller/UsuarioJpaController.java
@@ -187,6 +187,20 @@ public class UsuarioJpaController implements Serializable {
             em.close();
         }
     }
+    
+    public Usuario findUsuario(String login, String senha) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.createQuery("SELECT u FROM Usuario u WHERE u.login = :login AND u.senha = :senha", Usuario.class)
+                .setParameter("login", login)
+                .setParameter("senha", senha)
+                .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            em.close();
+        }
+    }
 
     public int getUsuarioCount() {
         EntityManager em = getEntityManager();
