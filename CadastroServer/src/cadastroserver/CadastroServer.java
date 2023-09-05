ff9acd4c4ce7a71b289eb0c52a1736090fa2d0a5
MariLFO <mari.fer.onorio@gmail.com>
2023-09-04 17:34:28 -0700

    Método main alterado

--- CadastroServer/src/cadastroserver/CadastroServer.java
+++ CadastroServer/src/cadastroserver/CadastroServer.java
@@ -4,6 +4,13 @@
  */
 package cadastroserver;
 
+import controller.ProdutoJpaController;
+import controller.UsuarioJpaController;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 /**
  *
  * @author Mari
@@ -13,8 +20,16 @@ public class CadastroServer {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("cadastroPU");
+        ProdutoJpaController ctrl = new ProdutoJpaController(emf);
+        UsuarioJpaController ctrlUsu = new UsuarioJpaController(emf);
+        ServerSocket serverSocket = new ServerSocket(4321);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            CadastroThread thread = new CadastroThread(ctrl, ctrlUsu, socket);
+            thread.start();
+        }
     }
-    
 }
